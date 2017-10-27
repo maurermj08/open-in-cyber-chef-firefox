cee947598d2e8bbbb27398e7ed2fd35f03fb7cd0
BuildTools <maurermj08@gmail.com>
2017-10-26 22:45:06 -0400

    Fixed the settings page and updated the icons

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 openInCyberChef = function(word){
   var query = word.selectionText;
-	browser.tabs.create({url: "https://gchq.github.io/CyberChef?input=" + encodeURIComponent(btoa(query))});
+  browser.storage.local.get("my_url", function(items) {
+	if (!!items.my_url) {
+		browser.tabs.create({url: items.my_url + "?input=" + encodeURIComponent(btoa(query))});
+	} else {
+		browser.tabs.create({url: "https://gchq.github.io/CyberChef?input=" + encodeURIComponent(btoa(query))});
+	}
+  });
 };
 
 browser.contextMenus.create({
-  id: "open-in-cyberchef",
   title: "Open in CyberChef",
   contexts:["selection"],
   onclick: openInCyberChef
--- options.js
+++ options.js
@@ -1,28 +1,23 @@
-// Saves options to chrome.storage
-function save_options() {
-  var custom_url = document.getElementById('cyberchef_url').value;
-  console.log("SET")
-  chrome.storage.sync.set({
-    my_url: custom_url
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.local.set({
+    my_url: document.querySelector("#my_url").value
   });
 }
 
-// Restores default value
-function restore_options() {
-  console.log("RESET!");
-  chrome.storage.sync.get({
-    my_url: 'https://gchq.github.io/CyberChef'
-  }, function(items) {
-    document.getElementById('cyberchef_url').value = items.my_url;
-  });
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#my_url").value = result.my_url || "https://gchq.github.io/CyberChef";
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var getting = browser.storage.local.get("my_url");
+  getting.then(setCurrentChoice, onError);
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('reset').addEventListener('click', restore_options);
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
