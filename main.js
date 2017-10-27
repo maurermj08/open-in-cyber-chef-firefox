25739fd68dacf7fa89adfb28f17777617fb4daa1
BuildTools <maurermj08@gmail.com>
2017-10-27 07:37:36 -0400

    Updated URL to use fragment identifier. Updated version.

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ openInCyberChef = function(word){
   var query = word.selectionText;
   browser.storage.local.get("my_url", function(items) {
 	if (!!items.my_url) {
-		browser.tabs.create({url: items.my_url + "?input=" + encodeURIComponent(btoa(query))});
+		browser.tabs.create({url: items.my_url + "#input=" + encodeURIComponent(btoa(query))});
 	} else {
-		browser.tabs.create({url: "https://gchq.github.io/CyberChef?input=" + encodeURIComponent(btoa(query))});
+		browser.tabs.create({url: "https://gchq.github.io/CyberChef#input=" + encodeURIComponent(btoa(query))});
 	}
   });
 };
